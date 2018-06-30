607008ba00b27cdf4a324b57f4b5e7137789fcf8
Raymond Cabrera <rjgcengr@gmail.com>
2018-06-30 16:46:10 -0700

    Set state of app after api request

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,10 @@
-export default function(state = null, action) {
-  return state
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // return state.concat([ action.payload.data ]); // concat doesn't mutate state, but creates new state instance
+    return [ action.payload.data, ...state ]; // replace with spread operator
+  }
+  return state;
 }
